48e549cd76afad778341c96078f284617a40c273
IIIBreakeRIII <ryu990305@icloud.com>
2023-06-03 23:40:10 +0900

    Clustering

--- data_load.py
+++ data_load.py
@@ -17,14 +17,15 @@ image_list_numpy = []
 
 for i in image_list:
     img = Image.open(dir_ + i)
-    image_array = np.array(img)
+    img_resize = img.resize((32, 32))
+    image_array = np.array(img_resize)
     image_list_numpy.append(image_array)
 
 # 리스트 전체 변환
 image_numpy = np.array(image_list_numpy)
 print(image_numpy.shape)    # (1142(개 데이터), 1134(가로 픽셀), 1134(세로 픽셀), 3(RGB))
 
-image_2D = image_numpy.reshape(-1, 1134 * 1134)
+image_2D = image_numpy.reshape(-1, 32 * 32)
 
 # plt.imshow(image_numpy[1], cmap='gray')
 # plt.show()
