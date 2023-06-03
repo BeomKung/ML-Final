c51dc08a7d85bb4d55aaeada92b9b4fc8e3b56bc
IIIBreakeRIII <ryu990305@icloud.com>
2023-06-04 00:10:24 +0900

    Cluster

--- clustering.py
+++ clustering.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-from data_load import image_2D as image
+from data_load import image_2D
+from data_load import image_numpy
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
@@ -10,7 +12,25 @@ from sklearn.decomposition import PCA
 # print(pca.components_.shape)
 
 km = KMeans(n_clusters=4, random_state=42, n_init=10)
-km.fit(image)
+km.fit(image_2D)
 
 print(km.labels_)
 print(np.unique(km.labels_, return_counts=True))
+
+def draw_clusters(arr, ratio=1):
+    n = len(arr)
+    rows = int(np.ceil(n / 10))
+    cols = n if rows < 2 else 10
+    fig, axs = plt.subplots(rows, cols, figsize=(cols * ratio, rows * ratio), squeeze=False)
+    
+    for i in range(rows):
+        for j in range(cols):
+            if i * 10 + j < n:
+                axs[i, j].imshow(arr[i * 10 + j])
+            axs[i, j].axis('off')
+    plt.show()
+
+draw_clusters(image_numpy[km.labels_==0])
+draw_clusters(image_numpy[km.labels_==1])
+draw_clusters(image_numpy[km.labels_==2])
+draw_clusters(image_numpy[km.labels_==3])
