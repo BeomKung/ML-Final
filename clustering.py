5e0a3df74253f097d38b08de0b9380b0090b317a
IIIBreakeRIII <ryu990305@icloud.com>
2023-06-03 22:59:42 +0900

    clustering

--- clustering.py
+++ clustering.py
@@ -1,5 +1,11 @@
 from data_load import image_2D as image
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
-km = KMeans(n_clusters=4, random_state=42)
-km.fit(image)
+pca = PCA(n_components=500)
+pca.fit(image)
+
+print(pca.components_.shape)
+
+# km = KMeans(n_clusters=4, random_state=42)
+# km.fit(image)
